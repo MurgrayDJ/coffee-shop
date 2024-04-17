6241ad7986e6ca142ff3053196ffc1a9c79d5bd1
Murgray D. John <murgraydj@gmail.com>
2024-04-17 14:46:03 -0400

    Removes extra MODIFY and ALTER lines

--- src/main/resources/coffeeshop_DB_creation.sql
+++ src/main/resources/coffeeshop_DB_creation.sql
@@ -2,31 +2,24 @@ CREATE DATABASE coffee_shop;
 
 USE coffee_shop;
 
-CREATE TABLE IF NOT EXISTS coffee_types (
-	type_id INT AUTO_INCREMENT,
-    coffee_type VARCHAR(100) UNIQUE NOT NULL,
-    has_coffee TINYINT NOT NULL,
-    has_espresso TINYINT NOT NULL,
-    milk_type TINYINT NOT NULL,
-    has_other_ingredients TINYINT NOT NULL,
-    PRIMARY KEY(type_id)
-);
-
 CREATE TABLE IF NOT EXISTS milk_types (
-	milk_type_id INT AUTO_INCREMENT,
+    milk_type_id INT AUTO_INCREMENT,
     milk_type VARCHAR(100) UNIQUE NOT NULL,
     milk_texture VARCHAR(100),
     PRIMARY KEY(milk_type_id)
 );
 
-ALTER TABLE coffee_types
-RENAME TO hot_coffee_types;
+CREATE TABLE IF NOT EXISTS hot_coffee_types (
+    type_id INT AUTO_INCREMENT,
+    coffee_type VARCHAR(100) UNIQUE NOT NULL,
+    has_coffee TINYINT NOT NULL,
+    has_espresso TINYINT NOT NULL,
+    milk_type_id INT,
+    has_other_ingredients TINYINT NOT NULL,
+    PRIMARY KEY(type_id),
+    FOREIGN KEY (milk_type_id) REFERENCES milk_types(milk_type_id)
+);
 
-ALTER TABLE hot_coffee_types
-RENAME COLUMN milk_type TO milk_type_id;
 
-ALTER TABLE hot_coffee_types
-MODIFY COLUMN milk_type_id INT; 
-
-ALTER TABLE hot_coffee_types 
-ADD FOREIGN KEY (milk_type_id) REFERENCES milk_types(milk_type_id);
+SHOW COLUMNS FROM hot_coffee_types;
+SHOW COLUMNS FROM milk_types;
