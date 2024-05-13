187b65cc050bec501c9f115fb419d6684a443af0
murgraydj <murgraydj@gmail.com>
2024-05-13 16:09:54 -0400

    Add GetMapping to Controller

--- src/main/java/com/murgray/controller/CoffeeShopController.java
+++ src/main/java/com/murgray/controller/CoffeeShopController.java
@@ -1,8 +1,13 @@
 package com.murgray.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoffeeShopController {
     
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the Coffee Shop!";
+    }
 }
