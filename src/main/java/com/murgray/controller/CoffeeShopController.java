591375f5b42197b65747b69a9f29b90717763d7b
murgraydj <murgraydj@gmail.com>
2024-05-15 14:55:03 -0400

    Update Controller to display db call on screen.
    For now, it displays a simple query that's in the
    repository.

--- src/main/java/com/murgray/controller/CoffeeShopController.java
+++ src/main/java/com/murgray/controller/CoffeeShopController.java
@@ -1,13 +1,25 @@
 package com.murgray.controller;
 
+import java.util.List;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.murgray.service.CoffeeService;
 
 @RestController
 public class CoffeeShopController {
     
+    private final CoffeeService coffeeService;
+
+    public CoffeeShopController(CoffeeService coffeeService) {
+        this.coffeeService = coffeeService;
+    }
+
     @GetMapping("/")
-    public String index() {
-        return "Welcome to the Coffee Shop!";
+    public String index(Model model) {
+        List<Object[]> coffeeTypes = coffeeService.findCoffeeTypes();
+        model.addAttribute("coffeeTypes", coffeeTypes);
+        return "home";
     }
 }
