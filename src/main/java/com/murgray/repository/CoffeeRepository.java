901b33eb89d63056d5eb5e5c4964e9a6cf76a858
murgraydj <murgraydj@gmail.com>
2024-05-15 14:39:31 -0400

    Add HotCoffee entity import to repository

--- src/main/java/com/murgray/repository/CoffeeRepository.java
+++ src/main/java/com/murgray/repository/CoffeeRepository.java
@@ -1,7 +1,10 @@
 package com.murgray.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import com.murgray.entity.HotCoffeeType;
 
 public interface CoffeeRepository extends JpaRepository<HotCoffeeType, Long> {
     
