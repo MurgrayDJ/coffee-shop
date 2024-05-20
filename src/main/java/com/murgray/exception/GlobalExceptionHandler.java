a1a5ab56304c0076ffff76a6f6d714ef7d4a4dc5
murgraydj <murgraydj@gmail.com>
2024-05-20 18:29:16 -0400

    Refactor to reduce code duplication

--- src/main/java/com/murgray/exception/GlobalExceptionHandler.java
+++ src/main/java/com/murgray/exception/GlobalExceptionHandler.java
@@ -1,5 +1,5 @@
 package com.murgray.exception;
- 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,31 +14,27 @@ public class GlobalExceptionHandler {
  
     @ExceptionHandler(CustomExceptions.ResourceNotFoundException.class)
     public ModelAndView handleResourceNotFoundException(CustomExceptions.ResourceNotFoundException ex, WebRequest request, Model model) {
-        ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("timestamp", new Date());
-        modelAndView.addObject("message", ex.getMessage());
-        modelAndView.addObject("details", request.getDescription(false));
-        modelAndView.setStatus(HttpStatus.NOT_FOUND);
-        return modelAndView;
+        return handleException(HttpStatus.NOT_FOUND, ex, request, model);
     }
  
     @ExceptionHandler(CustomExceptions.BadRequestException.class)
     public ModelAndView handleBadRequestException(CustomExceptions.BadRequestException ex, WebRequest request, Model model) {
-        ModelAndView modelAndView = new ModelAndView("error");
-        modelAndView.addObject("timestamp", new Date());
-        modelAndView.addObject("message", ex.getMessage());
-        modelAndView.addObject("details", request.getDescription(false));
-        modelAndView.setStatus(HttpStatus.BAD_REQUEST);
-        return modelAndView;
+        return handleException(HttpStatus.BAD_REQUEST, ex, request, model);
     }
  
     @ExceptionHandler(Exception.class)
     public ModelAndView handleGlobalException(Exception ex, WebRequest request, Model model) {
+        return handleException(HttpStatus.INTERNAL_SERVER_ERROR, ex, request, model);
+    }
+ 
+    private ModelAndView handleException(HttpStatus status, Exception ex, WebRequest request, Model model) {
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("timestamp", new Date());
         modelAndView.addObject("message", ex.getMessage());
         modelAndView.addObject("details", request.getDescription(false));
-        modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        modelAndView.setStatus(status);
         return modelAndView;
     }
 }
+
+
